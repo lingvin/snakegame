b0d3ebef0cb056afdad3bce228d29f757333e0ec
pruneplume <juicepooding@naver.com>
2018-11-26 20:03:51 +0900

    Add files via upload

--- snake.c
+++ snake.c
@@ -1,23 +1,39 @@
-#include<stdio.h>
-#include<curses.h>
+#include <stdio.h>
+#include <time.h>
+#include <malloc.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/select.h>
 
 
+#define ROW 5
+#define COLUMN 5
 
 
+enum Vector{
+	UP, DOWN, LEFT, RIGHT
+};
 
 
+typedef struct element{
+	int row;
+	int column;
+}
 
+typedef struct snake{
 
-int main()
-{
+	element data;
 
+	struct snake * next;
+}snake;
 
+int layer[ROW][COLUMN];
+int total_layer[ROW][COLUMN];
 
+void main(void){
 
-
-
-
-
-
+	
 
 }
+
